b7c18d3d2d5a7ff389c6dcb754b199a4286ef7f8
Kawsar Ahmed <72383564+devkowsir@users.noreply.github.com>
2024-12-08 23:56:48 +0600

    feat: equipment route complete

--- src/routes/equipment.js
+++ src/routes/equipment.js
@@ -14,3 +14,37 @@ router.post("/", async (req, res) => {
     res.status(500).json({ message: "Error creating equipment", error });
   }
 });
+
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const data = await Equipment.findOne({ _id: id });
+    console.log(data);
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching equipment", error });
+  }
+});
+
+// Update Equipment
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedEquipment = await Equipment.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.status(200).json(updatedEquipment);
+  } catch (error) {
+    res.status(500).json({ message: "Error updating equipment", error });
+  }
+});
+
+// Delete Equipment
+router.delete("/:id", async (req, res) => {
+  console.log(req.params.id);
+  try {
+    await Equipment.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "Equipment deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting equipment", error });
+  }
+});
+
+module.exports = router;
