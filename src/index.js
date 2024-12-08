999f26b04c80a07d63db1c40d03211e53a0c2eee
Kawsar Ahmed <72383564+devkowsir@users.noreply.github.com>
2024-12-08 20:28:20 +0600

    chore: initial setup

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const { PORT, ORIGIN, MONGO_URI } = process.env;
+console.log({ PORT, ORIGIN, MONGO_URI });
+
+const app = express();
+
+// middlewares
+app.use(cors({ origin: ORIGIN }));
+app.use(express.json());
+
+// Routes
+
+app.get("/", (req, res) => {
+  res.send("Found me!");
+});
+
+// connect to mongodb
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("connected to mongodb");
+
+    app.listen(PORT || 5000, () => {
+      console.log(`Server running on port ${PORT || 5000}`);
+    });
+  })
+  .catch((e) => console.error("Mongodb connection error", error));
