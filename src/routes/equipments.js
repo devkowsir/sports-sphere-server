0878c3b3e34a75b63908e92fd38253fb93dfbf58
Kawsar Ahmed <72383564+devkowsir@users.noreply.github.com>
2024-12-09 19:43:22 +0600

    refactor: removed get by id route

--- src/routes/equipments.js
+++ src/routes/equipments.js
@@ -15,16 +15,4 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/:email", async (req, res) => {
-  const { email: userEmail } = req.params;
-
-  try {
-    const data = await Equipment.find({ userEmail }).sort({ createdAt: -1 });
-    console.log(data);
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json({ message: "Error fetching equipment", error });
-  }
-});
-
 module.exports = router;
