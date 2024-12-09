6635c6dbfef535acea26716f13c8a749304d1eb4
Kawsar Ahmed <72383564+devkowsir@users.noreply.github.com>
2024-12-09 16:51:35 +0600

    fix: customization typo fixed and customization schema updated

--- src/models/Equipment.js
+++ src/models/Equipment.js
@@ -1,5 +1,13 @@
 const mongoose = require("mongoose");
 
+const customizationSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    cost: { type: Number, required: true },
+  },
+  { _id: false }
+);
+
 const equipmentSchema = new mongoose.Schema({
   image: { type: String, required: true },
   itemName: { type: String, required: true },
@@ -7,7 +15,7 @@ const equipmentSchema = new mongoose.Schema({
   description: { type: String, required: true },
   price: { type: Number, required: true },
   rating: { type: Number, required: true },
-  customization: { type: Array, required: true },
+  customizations: [customizationSchema],
   processingTime: { type: String, required: true },
   stockStatus: { type: Number, required: true },
   userEmail: { type: String, required: true },
