fc331125844604720ce26dd740371c7931380c95
Kawsar Ahmed <72383564+devkowsir@users.noreply.github.com>
2024-12-09 21:39:57 +0600

    fix: showing all user equipments even when email is passed

--- src/routes/equipments.js
+++ src/routes/equipments.js
@@ -4,11 +4,12 @@ const Equipment = require("../models/Equipment");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const { sort } = req.query;
+  const { sort, email } = req.query;
   const sortOptions = sort ? { price: parseInt(sort) } : { createdAt: -1 };
+  const filterOptions = email ? { userEmail: email } : {};
 
   try {
-    const data = await Equipment.find().sort(sortOptions);
+    const data = await Equipment.find(filterOptions).sort(sortOptions);
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json({ message: "Error fetching equipment", error });
