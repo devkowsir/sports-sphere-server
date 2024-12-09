b2b44b642e8f118a31d6e0031aac83c5fc160a74
Kawsar Ahmed <72383564+devkowsir@users.noreply.github.com>
2024-12-09 19:41:13 +0600

    feat: backend sorting functionality added

--- src/routes/equipments.js
+++ src/routes/equipments.js
@@ -4,9 +4,11 @@ const Equipment = require("../models/Equipment");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
+  const { sort } = req.query;
+  const sortOptions = sort ? { price: parseInt(sort) } : { createdAt: -1 };
+
   try {
-    const data = await Equipment.find();
-    console.log(data);
+    const data = await Equipment.find().sort(sortOptions);
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json({ message: "Error fetching equipment", error });
@@ -14,10 +16,10 @@ router.get("/", async (req, res) => {
 });
 
 router.get("/:email", async (req, res) => {
-  const email = req.params.email;
+  const { email: userEmail } = req.params;
 
   try {
-    const data = await Equipment.find({ userEmail: email });
+    const data = await Equipment.find({ userEmail }).sort({ createdAt: -1 });
     console.log(data);
     res.status(200).json(data);
   } catch (error) {
